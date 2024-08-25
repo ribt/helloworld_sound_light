9bf3a1684908505d13803669cfb689d1c630f99f
ribt <>
2024-08-25 15:14:12 +0200

    organize code

--- server.py
+++ server.py
@@ -8,9 +8,7 @@ from constants import *
 from threading import Thread
 
 MAIN_COLOR = Color(*MAIN_COLOR)
-
-strip = PixelStrip(NUMB_TABLES*TABLE_SIZE, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-strip.begin()
+TOTAL_PIXELS = NUMB_TABLES * TABLE_SIZE
 
 class Animation(Enum):
     IDLE = 1
@@ -31,17 +29,12 @@ class TableState():
     def getTick(self):
         return self._tick
 
-table_states = [TableState(Animation.IDLE) for _ in range(NUMB_TABLES)]
-
 def decreaseBrightness(color, coefficient):
     assert coefficient >= 0 and coefficient <= 1
-
     r = int(color.r * coefficient)
     g = int(color.g * coefficient)
     b = int(color.b * coefficient)
-
     return Color(r, g, b)
-
 
 def point_anim(tick):
     if tick >= TABLE_SIZE * 10:
@@ -56,13 +49,15 @@ def range_anim(tick):
     colors = [Color(0, 255, 0) for _ in range(tick//20)] + [Color(0, 0, 0) for _ in range(TABLE_SIZE - tick//20)]
     return colors
 
-def computeTablePixels(state: TableState):
+def computeTablePixels(state: TableState, reversed=False):
     ret = None
     if state.getAnimation() == Animation.FLAG:
         ret = point_anim(state.getTick())
     elif state.getAnimation() == Animation.ROUND:
         ret = range_anim(state.getTick())
     state.tick()
+    if reversed and ret is not None:
+        ret = ret[::-1]
     return ret
 
 def adaptBrightnessToMaxCurrent(pixels):
@@ -76,38 +71,44 @@ def adaptBrightnessToMaxCurrent(pixels):
             for i in range(len(tablePixels)):
                 pixels[table*TABLE_SIZE+i] = decreaseBrightness(tablePixels[i], ratio)
 
-def loop():
-    tick = 0
-    coefficients = [randint(1, 100)/100 for _ in range(strip.numPixels())]
+# arguments are lists to keep state between calls
+def idleAnimation(tick=[0], coefficients=[randint(1, 100)/100 for _ in range(TOTAL_PIXELS)]):
+    if tick[0] % 10 == 0:
+        tick[0] = 0
+        coefficients.insert(0, coefficients.pop())
+    for i in range(len(coefficients)):
+        coefficients[i] = coefficients[i] + randint(-50, 50)/1000
+        coefficients[i] = max(0.3, min(0.9, coefficients[i]))
+    pixels = [decreaseBrightness(MAIN_COLOR, coefficient) for coefficient in coefficients]
+    tick[0] += 1
+    return pixels
+
+def animateTables(pixels):
+    for table in range(NUMB_TABLES):
+        if table_states[table].getAnimation() == Animation.IDLE:
+            continue
+        tablePixels = computeTablePixels(table_states[table], reversed=(table%2==1))
+        if tablePixels is None:
+            table_states[table] = TableState(Animation.IDLE)
+            continue
+        for i in range(TABLE_SIZE):
+            pixels[table*TABLE_SIZE+i] = tablePixels[i]
+
+def showPixels(strip, pixels):
+    adaptBrightnessToMaxCurrent(pixels)
+    for i in range(TOTAL_PIXELS):
+        strip.setPixelColor(i, pixels[i])
+    strip.show()
+
+def stripControl():
+    strip = PixelStrip(TOTAL_PIXELS, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
+
     while True:
-        # Fill pixels with idle animation
-        if tick % 10 == 0:
-            tick = 0
-            coefficients = [coefficients[-1]] + coefficients[:-1]
-        for i in range(len(coefficients)):
-            coefficients[i] = coefficients[i] + randint(-50, 50)/1000
-            coefficients[i] = max(0.3, min(0.9, coefficients[i]))
-        pixels = [decreaseBrightness(MAIN_COLOR, coefficient) for coefficient in coefficients]
-
-        # Override pixels with table animations
-        for table in range(NUMB_TABLES):
-            if table_states[table].getAnimation() == Animation.IDLE:
-                continue
-            tablePixels = computeTablePixels(table_states[table])
-            if tablePixels is None:
-                table_states[table] = TableState(Animation.IDLE)
-                continue
-            pixels = pixels[:table*TABLE_SIZE] + tablePixels + pixels[(table+1)*TABLE_SIZE:]
-
-        adaptBrightnessToMaxCurrent(pixels)
-
-        # Set pixels
-        for i in range(len(pixels)):
-            strip.setPixelColor(i, pixels[i])
-        strip.show()
-
+        pixels = idleAnimation()
+        animateTables(pixels)
+        showPixels(strip, pixels)
         sleep(0.01)
-        tick += 1
 
 app = flask.Flask(__name__)
 
@@ -130,6 +131,8 @@ def box_pwned():
     return 'OK'
 
 if __name__ == '__main__':
-    thread = Thread(target=loop)
+    table_states = [TableState(Animation.IDLE) for _ in range(NUMB_TABLES)]
+
+    thread = Thread(target=stripControl)
     thread.start()
     app.run(debug=False, host='0.0.0.0', port=80)
