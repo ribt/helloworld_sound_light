d6db0b4887bc5f647591e91082b26fbe7a6cf9fa
ribt <>
2024-09-12 18:10:40 +0200

    patch git synchro

--- launch.sh
+++ launch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-git pull --force
+git fetch
+
+# Try to backup local changes
+git branch -d backup || true
+git branch backup || true
+
+git reset --hard origin/main
+
 PYTHON_EXEC=$(pipenv --py)
 sudo "$PYTHON_EXEC" server.py
